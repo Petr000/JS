9ed7fc30d07636025c74b8651b7c105166d280a9
Petr000 <123490081+Petr000@users.noreply.github.com>
2023-01-24 17:47:22 +0100

    Add files via upload

---
+++ 01p-main.js
@@ -0,0 +1,16 @@
+class Pojistenci {
+    constructor { (argJmeno, argTelefon, argVek)
+        this.jmenoPacienta = argJmeno ;
+        this.telefon = argTelefon;
+        this.věk =argVek;
+    
+
+
+    }
+}
+
+let pojistovna1 = new Pojistenci("Jan Novák","731584972","31");
+let pojistovna2 = new Pojistenci("Josef Novák", "725458414","25");
+let pojistovna3 = new Pojistenci("Hanka Blatná, 603417895","42");
+let pojistovna4 = new Pojistenci("Lukáš Holub","601222331","31");
+
